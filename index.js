d90318384b62543378846509961ec70104b9b75f
Alejandro <Alex_bcn10@hotmail.es>
2022-03-12 18:57:37 +0100

    conexion a la BD aislada en carpeta config correcto

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 import express from "express";
 import routerMovies from "./movies/routerMovies.js";
 import routerUsers from "./users/routerUsers.js"
-import mongoose from "mongoose"
+import connection from "./config/BD.js";
 
 const app = express()
 
 app.use(express.json());
 
-app.listen(3003,()=> console.log('Server Up at port 3003'))
+app.listen(3003,()=> console.log('Server Up at port 3003'));
+
+// Connection DB
+connection();
+
 app.use('/movies', routerMovies);
 app.use('/users', routerUsers);
-
-mongoose.connect('mongodb://localhost:27017/Movies', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(()=> console.log('Mongoose-Server Up'))
-.catch((error)=> console.log(error))
