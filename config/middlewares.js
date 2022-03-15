2f29bd1c93a73865af178576436f0e3bb102dfc7
Alejandro <Alex_bcn10@hotmail.es>
2022-03-15 18:06:56 +0100

    creado archivo middlewares y aislado el auth y checkjwt

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,18 +1,41 @@
-import Users from "../users/modelUsers.js";
+import jwt from 'jsonwebtoken';
 
-const autentication = async (req,res,next) => {
-  console.info('middleware of autentication');
-// transform the autentication function in a middleware function
-// Compares the headers credentials vs BD credentials.
-   const userFound = await Users.findOne({
-    email: req.headers.email,
-    password: req.headers.password,
-  });
-  if (!userFound) {
-    res.status(401).send("You shall not pass");
-  } else {
-    next();
-  }
-  // Autorized, and continues in next middleWare
+// Middleware that give you a token giving email and password
+const auth = (req,res,/*next*/) => {
+  const token = jwt.sign({email: req.headers.email, password: req.headers.password }, 'secretkey')
+  console.log(token)
+  res.json(token) // return the generated token
+  //next(token);
 }
-export default autentication;
+
+// Middleware that decode your token for contrart the email and password are right!
+const checkJwt = (req,res,next) => {
+  let tokenToCheck = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsZXhAYXBpLmNvbSIsInBhc3N3b3JkIjoiYWxleCIsImlhdCI6MTY0NzM2Mzc1Nn0.2GF6OMQz3mHN2nPk3awdCJPy7g5Pq3lDW2ntAi4GlD4";
+  console.log(req.headers.email, 'req mail en checkjwt')
+  console.log(req.headers.password, 'req password en checkjwt')
+  
+  const decoded = jwt.verify(tokenToCheck, 'secretkey')
+
+  console.log(decoded);
+
+  res.json(decoded)
+}
+export {auth, checkJwt};
+
+
+// const autentication = async (req,res,next) => {
+//   console.info('middleware of autentication');
+// // transform the autentication function in a middleware function
+// // Compares the headers credentials vs BD credentials.
+//    const userFound = await Users.findOne({
+//     email: req.headers.email,
+//     password: req.headers.password,
+//   });
+//   if (!userFound) {
+//     res.status(401).send("You shall not pass");
+//   } else {
+//     next();
+//   }
+//   // Autorized, and continues in next middleWare
+// }
+// export default autentication;
--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import routerMovies from "./movies/routerMovies.js";
 import routerUsers from "./users/routerUsers.js"
 import connection from "./config/BD.js";
 import morgan from "morgan";
-import autentication from "./config/middlewares.js";
-import jwt from 'jsonwebtoken';
+import {auth, checkJwt} from "./config/middlewares.js";
+
 
 const app = express()
 
@@ -23,25 +23,8 @@ app.use(morgan('tiny'));
 // How to server up with express
 app.listen(3003,()=> console.info('Server Up at port 3003'));
 
-const auth = (req,res,/*next*/) => {
-    const token = jwt.sign({email: req.headers.email, password: req.headers.password }, 'secretkey')
-    console.log(token)
-    res.json(token)
-    //next(token);
-}
 
-const checkJwt = (tokenToCheck) => {
-    console.log(req.headers.email, 'req mail en checkjwt')
-    console.log(req.headers.password, 'req password en checkjwt')
-    
-    const decoded = jwt.verify(tokenToCheck, 'secretKey')
-
-    console.log(decoded.email)
-    console.log(decoded);
-
-    return decoded
-}
-app.use('/', autentication)
+app.use('/auth', checkJwt)
 
 // Routes defined
 app.use('/movies', routerMovies);
