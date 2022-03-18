677ce80869a75f9e037d7b1b627003cde23a3a59
AlexMonPe <alex_bcn10@hotmail.es>
2022-03-18 11:11:10 +0100

    cambiado orden en index listen abajo de todo

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ import connection from "./config/BD.js"; // CONNECTION OF DB
 import morgan from "morgan"; 
 import logger from "./src/lib/winston.js";
 import env from "dotenv";
+
 const app = express()
 
 // ENVIRONMENT VARS
 env.config();
 
 // CONNECTION DB
-connection();
+await connection();
+
+// PARSE BODY TO JSON TO INTERPRETATE IT
+app.use(express.json())
+// MORGAN TO SHOW LOGS
+app.use(morgan('combined', { stream: logger.stream }));
+
+// ENDPOINTS
+app.use('/movies', routerMovies); //ENDPOINT OF MOVIES
+app.use('/users', routerUsers); //ENDPOINT OF USERS
+app.use('/rent', routerRent); // ENDPOINT OF RENT
 
 
-app.set("PORT", process.env.PORT || 5000);
-
+app.set("port", process.env.PORT || 5000);
 
 // HOW TO SERVER UP IN EXPRESS
 app.listen(app.get("port"), () => { 
@@ -24,16 +34,6 @@ app.listen(app.get("port"), () => {
 })
 
 
-// PARSE BODY TO JSON TO INTERPRETATE IT
-app.use(express.json())
-// MORGAN TO SHOW LOGS
-app.use(morgan('combined', { stream: logger.stream }));
-
-
-// ENDPOINTS
-app.use('/movies', routerMovies); //ENDPOINT OF MOVIES
-app.use('/users', routerUsers); //ENDPOINT OF USERS
-app.use('/rent', routerRent); // ENDPOINT OF RENT
 
 
 
