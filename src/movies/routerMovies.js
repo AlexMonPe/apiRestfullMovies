d4b0e8d94c1e9ecca12e19f0032ae38ad9179481
Alejandro <Alex_bcn10@hotmail.es>
2022-04-11 18:52:36 +0200

    comment auth middleware for test all endpoints in react app

--- src/movies/routerMovies.js
+++ src/movies/routerMovies.js
@@ -4,18 +4,18 @@ const router = express.Router();
 import { getMoviebyId, getMovie, postMovie, deleteMovie, updateMovie } from "./controllerMovies.js"
 
 // GET BY ID WITH PATH PARAMS
-router.get('/:id', autentication(["administrator","client"]), getMoviebyId); // ADMINISTRATORS AND CLIENTS CAN GET A MOVIE BYID
+router.get('/:id', /*autentication(["administrator","client"]),*/ getMoviebyId); // ADMINISTRATORS AND CLIENTS CAN GET A MOVIE BYID
 
 //GET FILTERING BY ALL KEYS AND RETURN ALL MOVIES IF KEY NO EXISTS
 router.get('/', getMovie)// ALL CAN READ MOVIES
 
 //POST NEW MOVIES
-router.post('/', autentication(["administrator", "client"]), postMovie); // ADMINISTRATORS AND CLIENTS CAN CREATE A MOVIE
+router.post('/',/* autentication(["administrator", "client"]),*/ postMovie); // ADMINISTRATORS AND CLIENTS CAN CREATE A MOVIE
 
 // DELETE MOVIES
-router.delete('/:id',autentication(["administrator"]), deleteMovie); //ONLY ADMINISTRATOR CAN DELETE MOVIES
+router.delete('/:id',/*autentication(["administrator"]),*/ deleteMovie); //ONLY ADMINISTRATOR CAN DELETE MOVIES
 
 // UPDATE/PATCH A MOVIE
-router.patch('/:id',autentication(["administrador"]) , updateMovie); //ONLY ADMINISTRATOR CAN UPDATE MOVIES
+router.patch('/:id',/*autentication(["administrador"]) ,*/ updateMovie); //ONLY ADMINISTRATOR CAN UPDATE MOVIES
 
 export default router;
