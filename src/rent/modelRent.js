c49aa69cc742edb23e0ce3b30c801ba19ea00f28
Alejandro <Alex_bcn10@hotmail.es>
2022-04-11 18:55:37 +0200

    rent schema updated with new fields

--- src/rent/modelRent.js
+++ src/rent/modelRent.js
@@ -6,7 +6,9 @@ import Movies from "../movies/modelMovies.js";
 const Schema = new mongoose.Schema({
     idMovie: { type: mongoose.Schema.Types.ObjectId, ref:"Movies", required: true },
     idUser: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
-    date: { type: Date, default: new Date() }
+    rent_date: { type: Date, default: new Date(),
+    return_date: {type: Date},
+    totalPrice: {type: mongoose.Schema.Types.Decimal128, required: true } }
 });
 const Rent = mongoose.model('Rent', Schema);
 export default Rent;
