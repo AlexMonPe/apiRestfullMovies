2988bacea0ca7c8916d87c9eadf6e046623efd83
Alejandro <Alex_bcn10@hotmail.es>
2022-04-11 18:55:09 +0200

    new endpoints added for filtered rents queries

--- src/rent/routerRent.js
+++ src/rent/routerRent.js
@@ -1,9 +1,11 @@
 import express from "express";
 const router = express.Router();
-import {deleteRent, getRent, getRentById, postRent, updateRent } from "./controllerRent.js";
+import {deleteRent, getRent, getRentById, getRentbyMovie, getRentbyUser, postRent, updateRent } from "./controllerRent.js";
 
 router.get("/", getRent);
 router.get("/:id", getRentById)
+router.get("/byuser/:id", getRentbyUser)
+router.get("/bymovie/:id", getRentbyMovie)
 router.post("/", postRent);
 router.delete("/:id", deleteRent)
 router.patch("/:id", updateRent)
