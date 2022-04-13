19085380c20c3bf537232db84f5904efec2e077f
Alejandro <Alex_bcn10@hotmail.es>
2022-04-13 15:48:24 +0200

    changed login parameters to body instead headers

--- config/middlewares.js
+++ config/middlewares.js
@@ -27,12 +27,12 @@ const login = async (req, res, next) => {
   try {
 
     let userFound = await Users.findOne({
-        email: req.headers.email
+        email: req.body.email
       });
       //console.log(userFound.password, ' hashed pwd')
-    if (await compareHash(req.headers.password, userFound.password)) {
+    if (await compareHash(req.body.password, userFound.password)) {
       console.log(userFound)
-      const token = jwt.sign({email: req.headers.email, id: userFound._id, role: userFound.role }, process.env.SECRET_KEY)
+      const token = jwt.sign({email: req.body.email, id: userFound._id, role: userFound.role }, process.env.SECRET_KEY)
       res.json({token: token, id: userFound._id, role: userFound.role})
     } else {
       res.status(404).send("Email, password or role are wrong");
@@ -47,7 +47,7 @@ const login = async (req, res, next) => {
 const autentication = (rolesToCheck = null) => {
   return (req,res,next) => {
     try {
-      const userDecoded = jwt.verify(req.headers.token, process.env.SECRET_KEY)
+      const userDecoded = jwt.verify(req.body.token, process.env.SECRET_KEY)
       req.token = userDecoded
       if (rolesToCheck == null || rolesToCheck.find((role)=> role === userDecoded.role)){
         next();
