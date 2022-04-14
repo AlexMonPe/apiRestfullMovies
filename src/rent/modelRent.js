bd85895c7e4fe5fa53a929793281d56e149816fa
Alejandro <Alex_bcn10@hotmail.es>
2022-04-14 20:54:06 +0200

    changed findbyid in getrentbyuser

--- src/rent/modelRent.js
+++ src/rent/modelRent.js
@@ -13,8 +13,8 @@ const date = addDaysToDate(today,3)
 
 // Schema: Data structure of collection Users
 const Schema = new mongoose.Schema({
-    idMovie: { type: mongoose.Schema.Types.ObjectId(), ref:"Movies", required: true },
-    idUser: { type: mongoose.Schema.Types.ObjectId(), ref: "Users", required: true },
+    idMovie: { type: mongoose.Schema.Types.ObjectId, ref:"Movies", required: true },
+    idUser: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
     rent_date: { type: String, default: new Date().toLocaleDateString()},
     return_date: {type: String, default: date},
     totalPrice: Number,
