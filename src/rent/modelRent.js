ca12dcf4b7a5d88fcc1bc1c61e73b148ff67863f
Alejandro <Alex_bcn10@hotmail.es>
2022-04-14 18:27:40 +0200

    seed changed

--- src/rent/modelRent.js
+++ src/rent/modelRent.js
@@ -2,13 +2,22 @@ import mongoose from "mongoose";
 import Users from "../users/modelUsers.js";
 import Movies from "../movies/modelMovies.js";
 
+const addDaysToDate = (date, days) =>{
+    date.setDate(date.getDate() + days);
+    const dateFormated = date.toLocaleDateString()
+    return dateFormated;
+}
+
+const today= new Date()
+const date = addDaysToDate(today,3)
+
 // Schema: Data structure of collection Users
 const Schema = new mongoose.Schema({
     idMovie: { type: mongoose.Schema.Types.ObjectId, ref:"Movies", required: true },
     idUser: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
     rent_date: { type: String, default: new Date().toLocaleDateString()},
-    return_date: {type: String},
-    totalPrice: {type: Number}
+    return_date: {type: String, default: date},
+    totalPrice: Number,
 });
 const Rent = mongoose.model('Rent', Schema);
 export default Rent;
