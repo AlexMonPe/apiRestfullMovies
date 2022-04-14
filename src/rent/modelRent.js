97a77c09344870be6b27fdea26911913b6e00c08
Alejandro <Alex_bcn10@hotmail.es>
2022-04-14 18:00:17 +0200

    trying to fix model rent

--- src/rent/modelRent.js
+++ src/rent/modelRent.js
@@ -7,7 +7,7 @@ const Schema = new mongoose.Schema({
     idMovie: { type: mongoose.Schema.Types.ObjectId, ref:"Movies", required: true },
     idUser: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
     rent_date: { type: String, default: new Date().toLocaleDateString()},
-    return_date: {type: String, default: new Date().toLocaleDateString()},
+    return_date: {type: String},
     totalPrice: {type: Number}
 });
 const Rent = mongoose.model('Rent', Schema);
